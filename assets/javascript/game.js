3ed5b1eb4f5324ab9b35563194b54eedb7a04a95
WatersJS <jsamuel.waters@gmail.com>
2018-05-27 16:53:53 -0400

    game is functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,93 +5,102 @@ var objScoreKeeper = {
     emerald: 0,
     sapphire: 0,
     diamond: 0,
-};
-
-var gameHTML = `
-    <button id="ruby" class="button" value="`+ objScoreKeeper.ruby +`">
-        Ruby
-    </button>
-    
-    <button id="sapphire" class="button" value="`+ objScoreKeeper.sapphire +`">
-        Sapphire
-    </button>
-    
-    <button id="emerald" class="button" value="3">
-        Emerald
-    </button>
-    
-    <button id="diamond" class="button" value="4">
-        Diamond
-    </button>
-    `;
-
-    objScoreKeeper = {
-    playerScore: 0,
-    targetScore:  0,
-    ruby: 0,
-    emerald: 0,
-    sapphire: 0,
-    diamond: 0,
+    keepPlaying: true,
     startGame: function() {
-        //playerScore is reset at game start
-        this.playerScore = 0;
-
+        //playerScore and playerWin are reset at game start
+        objScoreKeeper.playerScore = 0;
+        objScoreKeeper.keepPlaying = true;
+        
         //set target score to a randomly selected value between 50 - 99
         //this.targetScore = Math.floor(Math.random * 50 + 50); 
-        this.targetScore = Math.floor(Math.random * 30);
-
+        objScoreKeeper.targetScore = Math.floor(Math.random() * 30);
+        
         //each crystal will have a specific pattern of points
-        this.ruby = Math.ceil(Math.random() * 5);
-        this.sapphire = Math.ceil(Math.random() * 4 + 3);
-        this.emerald = Math.ceil(Math.random() * 3 + 4);
+        objScoreKeeper.ruby = 1;//Math.ceil(Math.random() * 5);
+        objScoreKeeper.sapphire = Math.ceil(Math.random() * 4 + 3);
+        objScoreKeeper.emerald = Math.ceil(Math.random() * 3 + 4);
         objScoreKeeper.diamond = Math.ceil(Math.random() * 6 + 2);
-        console.log(this.ruby);
-        console.log(this.sapphire);
-        console.log(this.emerald);
+        console.log(objScoreKeeper.ruby);
+        console.log(objScoreKeeper.sapphire);
+        console.log(objScoreKeeper.emerald);
         console.log(objScoreKeeper.diamond);
+        
+        //!!! gameHTML needs to be declared after the crystals have had thier values randomized so that the buttons generated take the correct values
+        var gameHTML = `
+            <button id="ruby" class="button" value="`+ objScoreKeeper.ruby +`">
+                Ruby
+            </button>
+            
+            <button id="sapphire" class="button" value="`+ objScoreKeeper.sapphire +`">
+                Sapphire
+            </button>
+            
+            <button id="emerald" class="button" value="`+ objScoreKeeper.emerald +`">
+                Emerald
+            </button>
+            
+            <button id="diamond" class="button" value="`+ objScoreKeeper.diamond +`">
+                Diamond
+            </button>
+            `;
+
+        //!!! 
+        var scoreHTML = `
+            <div id="player-score">
+                Your Score: `+ objScoreKeeper.playerScore +`
+            </div>
+            <div id="target-score">
+                Target Score: `+ objScoreKeeper.targetScore +`
+            </div>
+            `;
         
         //display game content
         $("#game").html(gameHTML);
-        console.log(this.addPoints);
+        $("#score").html(scoreHTML);
+        $("#end").html('<br><br><div id="end"></div>')
         
-        //++!!!
-        //The html to which a listener applies must already exist before you try to assign it
-        //--!!!
+        //!!! The html to which a listener applies must already exist before you try to assign it
         //set event listener for gem clicks
         
         $(".button").on("click", objScoreKeeper.addPoints);
-
-
+    
         
     },
     
     addPoints: function(){
-        console.log("hello");
-        if (this.id == "ruby") {
-            objScoreKeeper.ruby += $(this).val(); //getting treated like a string
-            console.log(objScoreKeeper.ruby);
-        
-        } else if (this.id == "sapphire") {
-                console.log("hi sapphy");
 
-        } else if (this.id == "emerald") {
-                console.log("hi emmy");
+        var restartHTML = `
+        <button id="restart" class="button">
+            Play Again
+        </button>
+        `;
 
-        } else {
-                console.log("hi diva");
-        }
+        if (objScoreKeeper.keepPlaying == true) {
+            objScoreKeeper.playerScore += parseInt($(this).val());
+            if (objScoreKeeper.playerScore == objScoreKeeper.targetScore) {
+                // player wins
+                objScoreKeeper.keepPlaying = false;
+                $("#end").html("<h2>you win</h2>" + restartHTML);
+            } else if (objScoreKeeper.playerScore > objScoreKeeper.targetScore){
+                // player loses
+                objScoreKeeper.keepPlaying = false;
+                $("#end").html("<h2>You Lose</h2>" + restartHTML);
+            } else {
+                // player score is low enough to continue play
+                $("#end").html("<h2>keep going</h2>");
+            }
+            //update player score
+            $("#player-score").html(`
+            <div id="player-score">
+            Your Score: `+ objScoreKeeper.playerScore +`
+            </div>
+            `);
+        };
+
+        $("#restart").on("click", objScoreKeeper.startGame);
 
     },
     
-    checkScore: function(){
-        
-    }
 };
 
 $(document).ready(objScoreKeeper.startGame);
-
-// //the game executes below
-// $(document).ready(function() {
-//     $("#game").html(gameHTML);
-
-// });
